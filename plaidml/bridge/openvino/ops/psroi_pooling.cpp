b238cde1859d5c594fc3b8bbd90ba629702ed5a7
zhangding <ding.zhang@intel.com>
2021-04-12 06:58:26 -0400

    Optimize PSROIPooling

--- plaidml/bridge/openvino/ops/psroi_pooling.cpp
+++ plaidml/bridge/openvino/ops/psroi_pooling.cpp
@@ -39,6 +39,69 @@ edsl::Tensor single_bin_pooling(edsl::Tensor I, size_t ph, size_t pw, size_t gro
   return O;
 }
 
+edsl::Tensor extract_reduce_rois_dim(edsl::Tensor I) {
+  std::vector<edsl::TensorDim> I_dims(10), O_dims(8);
+  std::vector<edsl::TensorIndex> I_idxs(10), O_idxs(8);
+  I.bind_dims(I_dims);
+  for (size_t i = 0; i < 8; ++i) {
+    O_dims[i] = I_dims[i + 2];
+  }
+  for (size_t i = 0; i < 8; ++i) {
+    I_idxs[i + 2] = O_idxs[i];
+  }
+  I_idxs[0] = I_idxs[2];
+  edsl::Tensor O = Contraction(O_dims, O_idxs).assign(I(I_idxs));
+  return O;
+}
+
+edsl::Tensor extract_reduce_other_dims(edsl::Tensor I) {
+  std::vector<edsl::TensorDim> I_dims(13), O_dims(7);
+  std::vector<edsl::TensorIndex> I_idxs(13), O_idxs(7);
+  I.bind_dims(I_dims);
+  for (size_t i = 0; i < 7; ++i) {
+    O_dims[i] = I_dims[i + 6];
+  }
+  for (size_t i = 0; i < 7; ++i) {
+    I_idxs[i + 6] = O_idxs[i];
+  }
+  for (size_t i = 0; i < 6; ++i) {
+    I_idxs[i] = I_idxs[i + 6];
+  }
+  edsl::Tensor O = Contraction(O_dims, O_idxs).assign(I(I_idxs));
+  return O;
+}
+
+edsl::Tensor extract_tensor(edsl::Tensor I) {
+  std::vector<edsl::TensorDim> I_dims(12), O_dims(6);
+  std::vector<edsl::TensorIndex> I_idxs(12), O_idxs(6);
+  I.bind_dims(I_dims);
+  for (size_t i = 0; i < 6; ++i) {
+    O_dims[i] = I_dims[i];
+  }
+  for (size_t i = 0; i < 6; ++i) {
+    I_idxs[i] = O_idxs[i];
+  }
+  for (size_t i = 0; i < 6; ++i) {
+    I_idxs[i + 6] = I_idxs[i];
+  }
+  edsl::Tensor O = Contraction(O_dims, O_idxs).assign(I(I_idxs));
+  return O;
+}
+
+edsl::Tensor compute_psroi_pooling(edsl::Tensor I) {
+  std::vector<edsl::TensorDim> I_dims(6), O_dims(4);
+  std::vector<edsl::TensorIndex> I_idxs(6), O_idxs(4);
+  I.bind_dims(I_dims);
+  for (size_t i = 0; i < 4; ++i) {
+    O_dims[i] = I_dims[i];
+  }
+  for (size_t i = 0; i < 4; ++i) {
+    I_idxs[i] = O_idxs[i];
+  }
+  edsl::Tensor O = Contraction(O_dims, O_idxs).sum(I(I_idxs));
+  return O;
+}
+
 }  // namespace
 
 namespace PlaidMLPlugin {
@@ -50,6 +113,7 @@ void registerPSROIPooling() {
     auto* layer = ngraph::as_type<ngraph::opset5::PSROIPooling>(ctx.layer);
     IE_ASSERT(ctx.operands.size() == 2);
     auto I = ctx.operands.at(0);  // Input
+    auto C = ctx.operands.at(1);  // Coord
 
     std::vector<float> coords = cast_constant_operand<float>(1, layer);
     IE_ASSERT((coords.size() % BOX_ELEMENT_SIZE) == 0);
@@ -117,71 +181,75 @@ void registerPSROIPooling() {
       if (output_dim * spatial_bins_x * spatial_bins_y != channel_in) {
         THROW_IE_EXCEPTION << "Incorrected channel of the input tensor.";
       }
-      std::vector<edsl::Tensor> bin_concat_channel_vec;
-      for (size_t roi = 0; roi < num_rois; ++roi) {
-        // Get the start and end coordinate of the box.
-        auto batch_id = coords[roi * BOX_ELEMENT_SIZE];
-        auto start_w = coords[roi * BOX_ELEMENT_SIZE + 1];
-        auto start_h = coords[roi * BOX_ELEMENT_SIZE + 2];
-        auto end_w = coords[roi * BOX_ELEMENT_SIZE + 3];
-        auto end_h = coords[roi * BOX_ELEMENT_SIZE + 4];
-        start_w = start_w * spatial_scale;
-        start_h = start_h * spatial_scale;
-        end_w = end_w * spatial_scale;
-        end_h = end_h * spatial_scale;
-        auto box_width = end_w - start_w;
-        auto box_height = end_h - start_h;
-        auto bin_width = box_width / spatial_bins_x;
-        auto bin_height = box_height / spatial_bins_y;
-        float width_scale = 0;
-        float height_scale = 0;
-        if (pooling_width > 1) width_scale = bin_width * (width - 1) / (pooling_width - 1);
-        if (pooling_height > 1) height_scale = bin_height * (height - 1) / (pooling_height - 1);
-        std::vector<edsl::Tensor> bin_concat_height_vec;
-        for (size_t c_out = 0; c_out < output_dim; c_out++) {
-          std::vector<edsl::Tensor> bin_concat_width_vec;
-          for (size_t ph = 0; ph < pooling_height; ph++) {
-            std::vector<edsl::Tensor> single_bin_vec;
-            for (size_t pw = 0; pw < pooling_width; pw++) {
-              auto single_bin = edsl::cast(edsl::index({edsl::TensorDim(1)}, 0), DType::FLOAT32);
-              for (size_t sby = 0; sby < spatial_bins_y; sby++) {
-                for (size_t sbx = 0; sbx < spatial_bins_x; sbx++) {
-                  auto bin_start_w = start_w + sbx * bin_width;
-                  auto bin_start_h = start_h + sby * bin_height;
+      // Get the start and end coordinate of the box.
+      edsl::Tensor batch_id = op::slice(C).add_dim(0, num_rois).add_dim(0, 1);
+      edsl::Tensor start_w = op::slice(C).add_dim(0, num_rois).add_dim(1, 2);
+      edsl::Tensor start_h = op::slice(C).add_dim(0, num_rois).add_dim(2, 3);
+      edsl::Tensor end_w = op::slice(C).add_dim(0, num_rois).add_dim(3, 4);
+      edsl::Tensor end_h = op::slice(C).add_dim(0, num_rois).add_dim(4, 5);
+      start_w = start_w * spatial_scale;
+      start_h = start_h * spatial_scale;
+      end_w = end_w * spatial_scale;
+      end_h = end_h * spatial_scale;
+      edsl::Tensor box_width = end_w - start_w;
+      edsl::Tensor box_height = end_h - start_h;
+      edsl::Tensor bin_width = box_width / spatial_bins_x;
+      edsl::Tensor bin_height = box_height / spatial_bins_y;
+      edsl::Tensor width_scale = edsl::cast(edsl::index({edsl::TensorDim(1)}, 0), DType::FLOAT32);
+      edsl::Tensor height_scale = edsl::cast(edsl::index({edsl::TensorDim(1)}, 0), DType::FLOAT32);
+      if (pooling_width > 1) width_scale = bin_width * (width - 1) / (pooling_width - 1);
+      if (pooling_height > 1) height_scale = bin_height * (height - 1) / (pooling_height - 1);
 
-                  auto point_x = pooling_width > 1 ? (pw * width_scale + bin_start_w * (width - 1))
-                                                   : (bin_start_w + bin_start_w + bin_width) * (width - 1) / 2;
-                  auto point_y = pooling_height > 1 ? (ph * height_scale + bin_start_h * (height - 1))
-                                                    : (bin_start_h + bin_start_h + bin_height) * (height - 1) / 2;
-                  if (point_x < width && point_y < height) {
-                    auto c_in = edsl::cast(edsl::index({edsl::TensorDim(1)}, 0), DType::FLOAT32) +
-                                (sby * spatial_bins_x + sbx) * num_classes + c_out;
-                    auto batch_id_tensor = edsl::cast(edsl::index({edsl::TensorDim(1)}, 0), DType::FLOAT32) + batch_id;
-                    auto point_x_tensor = edsl::cast(edsl::index({edsl::TensorDim(1)}, 0), DType::FLOAT32) + point_x;
-                    auto point_y_tensor = edsl::cast(edsl::index({edsl::TensorDim(1)}, 0), DType::FLOAT32) + point_y;
-                    // Get the single sub bin with bilinear interpolate.
-                    edsl::Tensor single_sub_bin = edsl::gather(I, batch_id_tensor).axis(0);
-                    single_sub_bin = edsl::gather(single_sub_bin, c_in).axis(1);
-                    single_sub_bin = edsl::gather(single_sub_bin, point_y_tensor).axis(2);
-                    single_sub_bin = edsl::gather(single_sub_bin, point_x_tensor).axis(3);
-                    single_bin = single_bin + single_sub_bin;
-                  }
-                }
-              }
-              single_bin = single_bin / (spatial_bins_x * spatial_bins_y);
-              single_bin = op::reshape(single_bin, make_tuple<int64_t>({1, 1, 1, 1}));
-              single_bin_vec.push_back(single_bin);
-            }
-            auto bin_concat_width = op::concatenate(single_bin_vec, 3);
-            bin_concat_width_vec.push_back(bin_concat_width);
-          }
-          auto bin_concat_height = op::concatenate(bin_concat_width_vec, 2);
-          bin_concat_height_vec.push_back(bin_concat_height);
-        }
-        auto bin_concat_channel = op::concatenate(bin_concat_height_vec, 1);
-        bin_concat_channel_vec.push_back(bin_concat_channel);
-      }
-      output = op::concatenate(bin_concat_channel_vec, 0);
+      start_w = op::reshape(start_w, make_tuple<size_t>({num_rois, 1, 1, 1, 1, 1}));
+      start_h = op::reshape(start_h, make_tuple<size_t>({num_rois, 1, 1, 1, 1, 1}));
+      bin_width = op::reshape(bin_width, make_tuple<size_t>({num_rois, 1, 1, 1, 1, 1}));
+      bin_height = op::reshape(bin_height, make_tuple<size_t>({num_rois, 1, 1, 1, 1, 1}));
+      width_scale = op::reshape(width_scale, make_tuple<size_t>({num_rois, 1, 1, 1, 1, 1}));
+      height_scale = op::reshape(height_scale, make_tuple<size_t>({num_rois, 1, 1, 1, 1, 1}));
+
+      edsl::Tensor c_out = edsl::cast(edsl::index({edsl::TensorDim(num_rois), edsl::TensorDim(output_dim),
+                                                   edsl::TensorDim(pooling_height), edsl::TensorDim(pooling_width),
+                                                   edsl::TensorDim(spatial_bins_y), edsl::TensorDim(spatial_bins_x)},
+                                                  1),
+                                      DType::FLOAT32);
+      edsl::Tensor ph = edsl::cast(edsl::index({edsl::TensorDim(num_rois), edsl::TensorDim(output_dim),
+                                                edsl::TensorDim(pooling_height), edsl::TensorDim(pooling_width),
+                                                edsl::TensorDim(spatial_bins_y), edsl::TensorDim(spatial_bins_x)},
+                                               2),
+                                   DType::FLOAT32);
+      edsl::Tensor pw = edsl::cast(edsl::index({edsl::TensorDim(num_rois), edsl::TensorDim(output_dim),
+                                                edsl::TensorDim(pooling_height), edsl::TensorDim(pooling_width),
+                                                edsl::TensorDim(spatial_bins_y), edsl::TensorDim(spatial_bins_x)},
+                                               3),
+                                   DType::FLOAT32);
+      edsl::Tensor sby = edsl::cast(edsl::index({edsl::TensorDim(num_rois), edsl::TensorDim(output_dim),
+                                                 edsl::TensorDim(pooling_height), edsl::TensorDim(pooling_width),
+                                                 edsl::TensorDim(spatial_bins_y), edsl::TensorDim(spatial_bins_x)},
+                                                4),
+                                    DType::FLOAT32);
+      edsl::Tensor sbx = edsl::cast(edsl::index({edsl::TensorDim(num_rois), edsl::TensorDim(output_dim),
+                                                 edsl::TensorDim(pooling_height), edsl::TensorDim(pooling_width),
+                                                 edsl::TensorDim(spatial_bins_y), edsl::TensorDim(spatial_bins_x)},
+                                                5),
+                                    DType::FLOAT32);
+      edsl::Tensor bin_start_w = start_w + sbx * bin_width;
+      edsl::Tensor bin_start_h = start_h + sby * bin_height;
+      edsl::Tensor point_x = pooling_width > 1 ? (pw * width_scale + bin_start_w * (width - 1))
+                                               : (bin_start_w + bin_start_w + bin_width) * (width - 1) / 2;
+      edsl::Tensor point_y = pooling_height > 1 ? (ph * height_scale + bin_start_h * (height - 1))
+                                                : (bin_start_h + bin_start_h + bin_height) * (height - 1) / 2;
+      edsl::Tensor c_in = (sby * spatial_bins_x + sbx) * num_classes + c_out;
+
+      output = edsl::gather(I, batch_id).axis(0);
+      output = edsl::gather(output, c_in).axis(-3);
+      output = extract_reduce_rois_dim(output);
+      output = edsl::gather(output, point_y).axis(-2);
+      output = extract_reduce_other_dims(output);
+      output = edsl::gather(output, point_x).axis(-1);
+      output = extract_tensor(output);
+      output = compute_psroi_pooling(output);
+      output = output / (spatial_bins_x * spatial_bins_y);
+
     } else {
       THROW_IE_EXCEPTION << "Invalid PS ROI pooling mode.";
     }
--- plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/psroi_pooling.cpp
+++ plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/psroi_pooling.cpp
@@ -20,29 +20,29 @@ const std::vector<float> spatial_scale = {1.0, 0.5};
 const std::vector<std::string> mode = {"average", "bilinear"};
 
 INSTANTIATE_TEST_CASE_P(smoke_PSROIPooling, PSROIPoolingLayerTest,
-                        ::testing::Combine(::testing::Values(std::vector<size_t>({1, 8, 5, 5})),  //
-                                           ::testing::ValuesIn(coords),                           //
-                                           ::testing::Values(2),                                  //
-                                           ::testing::Values(2),                                  //
-                                           ::testing::ValuesIn(spatial_scale),                    //
-                                           ::testing::Values(2),                                  //
-                                           ::testing::Values(2),                                  //
-                                           ::testing::ValuesIn(mode),                             //
-                                           ::testing::ValuesIn(netPrecisions),                    //
-                                           ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),   //
+                        ::testing::Combine(::testing::Values(std::vector<size_t>({1, 8, 10, 10})),  //
+                                           ::testing::ValuesIn(coords),                             //
+                                           ::testing::Values(2),                                    //
+                                           ::testing::Values(2),                                    //
+                                           ::testing::ValuesIn(spatial_scale),                      //
+                                           ::testing::Values(2),                                    //
+                                           ::testing::Values(2),                                    //
+                                           ::testing::ValuesIn(mode),                               //
+                                           ::testing::ValuesIn(netPrecisions),                      //
+                                           ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),     //
                         PSROIPoolingLayerTest::getTestCaseName);
 
 INSTANTIATE_TEST_CASE_P(PSROIPooling, PSROIPoolingLayerTest,
-                        ::testing::Combine(::testing::Values(std::vector<size_t>({1, 18, 6, 6})),  //
-                                           ::testing::ValuesIn(coords),                            //
-                                           ::testing::Values(2),                                   //
-                                           ::testing::Values(3),                                   //
-                                           ::testing::ValuesIn(spatial_scale),                     //
-                                           ::testing::Values(3),                                   //
-                                           ::testing::Values(3),                                   //
-                                           ::testing::ValuesIn(mode),                              //
-                                           ::testing::ValuesIn(netPrecisions),                     //
-                                           ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),    //
+                        ::testing::Combine(::testing::Values(std::vector<size_t>({1, 18, 10, 10})),  //
+                                           ::testing::ValuesIn(coords),                              //
+                                           ::testing::Values(2),                                     //
+                                           ::testing::Values(3),                                     //
+                                           ::testing::ValuesIn(spatial_scale),                       //
+                                           ::testing::Values(3),                                     //
+                                           ::testing::Values(3),                                     //
+                                           ::testing::ValuesIn(mode),                                //
+                                           ::testing::ValuesIn(netPrecisions),                       //
+                                           ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),      //
                         PSROIPoolingLayerTest::getTestCaseName);
 
 }  // namespace
